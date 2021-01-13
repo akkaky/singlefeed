e09297a5baae9527a950883ce659efe452128538
Nikolay <korotin.nikolay@outlook.com>
2021-01-13 20:13:32 +0300

    update main.py

--- main.py
+++ main.py
@@ -99,8 +99,30 @@ def init():
     return settings
 
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    feeds = storage.get_feeds()
+    return render_template('index.html', feeds=feeds)
+
+
+@app.route('/<feed_name>')
+def feed_page(feed_name):
+    feed = storage.get_feeds(feed_name)
+    sort_episodes(feed)
+    return render_template('feed_page.html', feed=feed)
+
+
+@app.route('/rss/<feed_name>')
+def rss(feed_name):
+    feed = storage.get_feeds(feed_name)
+    sort_episodes(feed)
+    return Response(create_rss(feed), mimetype='text/xml')
+
+
 def main():
-    app = Flask(__name__)
     settings = init()
     scheduler = BackgroundScheduler()
     scheduler.start()
@@ -108,26 +130,8 @@ def main():
         update_feeds, trigger="interval", seconds=int(settings.get('timeout'))
     )
 
-    @app.route('/')
-    def index():
-        feeds = storage.get_feeds()
-        return render_template('index.html', feeds=feeds)
-
-    @app.route('/<feed_name>')
-    def feed_page(feed_name):
-        feed = storage.get_feeds(feed_name)
-        sort_episodes(feed)
-        return render_template('feed_page.html', feed=feed)
-
-    @app.route('/rss/<feed_name>')
-    def rss(feed_name):
-        feed = storage.get_feeds(feed_name)
-        sort_episodes(feed)
-        return Response(create_rss(feed), mimetype='text/xml')
-
-    return app
 
 
 if __name__ == '__main__':
-    app = main()
+    main()
     app.run()
