3299f7b954e73140eb18a08c01d0faccb41a5404
Nikolay <korotin.nikolay@outlook.com>
2021-01-14 12:00:15 +0300

    update main.py

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ def main():
     scheduler.add_job(
         update_feeds, trigger="interval", seconds=int(settings.get('timeout'))
     )
-
+    return app.run()
 
 if __name__ == '__main__':
     main()
